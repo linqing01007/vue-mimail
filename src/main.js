2e0685f0361425559e70136ab0b8222c2e21a42a
nhq <miracle_xiu@163.com>
2020-09-18 22:26:10 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import vueAxios from 'vue-axios'
+import App from './App.vue'
 
+axios.interceptors.request.use(function (config) {
+  console.log('axios interceptors request: ', config)
+})
+//接口错误拦截
+axios.interceptors.response.use(function (response) {
+  console.log('axios interceptors response: ', response)
+  let res = response.data
+  if (res.status == 0) {
+    return res.data
+  } else if (res.status == 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
 Vue.config.productionTip = false
 
 new Vue({
