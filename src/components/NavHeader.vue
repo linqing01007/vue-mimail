72fe1fb4b5978bc82190e207a9a01ff45c2bd1d9
nhq <miracle_xiu@163.com>
2020-11-23 23:44:46 +0800

    提交修改

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -138,7 +138,8 @@ export default {
     getProductList () {
       this.axios.get('/products', {
         params: {
-          categoryId: '100012'
+          categoryId: '100012',
+          pageSize: 6
         }
       }).then(res => {
         this.productList = res.list.slice(0, 6)
@@ -244,8 +245,8 @@ export default {
               box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
               overflow: hidden;
               transition: all .5s;
-              // z-index: 10;
-              // background-color: #ffffff;
+              z-index: 10;
+              background-color: #ffffff;
               .product {
                 position: relative;
                 float: left;
